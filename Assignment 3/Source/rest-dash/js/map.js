a82c78fba2a29bf80509c3828c30fb387b57da23
jreijrink <j.reijrink@student.tue.nl>
2015-01-22 17:18:25 +0100

    changes xD

--- Assignment 3/Source/rest-dash/js/map.js
+++ Assignment 3/Source/rest-dash/js/map.js
@@ -1,150 +1,20 @@
 //http://leafletjs.com/examples/quick-start.html
 //http://bl.ocks.org/Caged/6476579
 
-var map = L.map('map');
-var data = [];
+var map = L.map('map', { fullscreenControl: true });
 var defaultIcon;
 var selectedIcon;
+var userIcon;
+var userSelectedIcon;
 var clickedMarker;
-var markers = [];
+var clickedUserMarker;
+var restaurantMarkers = [];
+var userMarkers = [];
 var restaurantRatings = [];
-	
-L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
-	maxZoom: 18,
-	id: 'examples.map-i875mjb7'
-}).addTo(map);
-
-//var popup = L.popup();
-function onMapClick(e) {	
-	if(clickedMarker)
-	{
-		clickedMarker.setIcon(defaultIcon);
-	}	
-	
-	if(e.target.options.data)
-	{
-		e.target.setIcon(selectedIcon);
-		clickedMarker = e.target;
-		
-		var entry = e.target.options.data;
-		selectedMapMarker(entry);
-		
-		showInfo(entry);
-	}
-	else
-	{
-		nothingSelected();
-	}
-}
-
-queue()
-   .defer(d3.csv, "data/geoplaces2.csv")
-   .defer(d3.csv, "data/rating_final.csv")
-   .await(dataLoaded);
-
-function dataLoaded(error, geoData, userRatings) {	
-	data = geoData;
-	map.on('click', onMapClick);
-	
-	initRestaurantRatings(data);
-	fillRestaurantRatings(userRatings);
-	createBarChart();
-	nothingSelected();
-	
-	var restaurantSelection = document.getElementById("restaurantSelection");		
-	var bounds = [];
-	var BlueIcon = L.Icon.Default.extend({
-		options: {
-				iconUrl: 'img/marker-icon-blue.png' 
-		}
-	 });
-	var RedIcon = L.Icon.Default.extend({
-		options: {
-				iconUrl: 'img/marker-icon-red.png' 
-		}
-	 });
-	 selectedIcon = new RedIcon();
-	 defaultIcon = new BlueIcon();
-	 
-	data.forEach(function(entry) {
-		var lat = entry.latitude;
-		var lon = entry.longitude;
-		var name = entry.name;
-		var placeID = entry.placeID;
-		var coordinate = L.latLng(lat, lon);
-		
-		var marker = L.marker(coordinate, {
-            data: entry,
-			icon: defaultIcon
-        });
-		//marker.addTo(map).bindPopup("<b>Hello world!</b><br />I am " + name + " .").on('click', onMapClick);	
-		marker.addTo(map).on('click', onMapClick);			
-			
-		bounds[bounds.length] = coordinate;
-		markers[markers.length] = marker;
-		
-		var option = document.createElement("option");
-		option.value = placeID;
-		option.data = entry;
-		option.text = name;
-		restaurantSelection.add(option);
-	});	
-	map.fitBounds(bounds);
-}
-
-function selectRestaurant(placeID) {
-	if(clickedMarker)
-	{
-		clickedMarker.setIcon(defaultIcon);
-		//clickedMarker.closePopup();
-	}
-	markers.forEach(function(marker) {
-		if(placeID == marker.options.data.placeID)
-		{
-			marker.setIcon(selectedIcon);
-			map.panTo(marker._latlng);
-			
-			//marker.openPopup();
-			clickedMarker = marker;
-	
-			showInfo(marker.options.data);
-		}
-	});
-}
-
-function selectedMapMarker(restaurant)
-{    
-    var element = document.getElementById('restaurantSelection');
-    element.value = restaurant.placeID;
-
-	showInfo(restaurant);
-}
-
-function initRestaurantRatings(restaurants)
-{
-	data.forEach(function(entry) {
-		restaurantRatings[entry.placeID] =  {count:0, overall:0, service:0, food:0, overallData:[], serviceData:[], foodData:[] };
-	});
-}
-
-function fillRestaurantRatings(userRatings)
-{
-	userRatings.forEach(function(userRating) {
-		restaurantRatings[userRating.placeID].count++;
-		restaurantRatings[userRating.placeID].overall += parseInt(userRating.rating);
-		restaurantRatings[userRating.placeID].service += parseInt(userRating.service_rating);
-		restaurantRatings[userRating.placeID].food += parseInt(userRating.food_rating);
-		
-		restaurantRatings[userRating.placeID].overallData[restaurantRatings[userRating.placeID].overallData.length] = parseInt(userRating.rating);
-		restaurantRatings[userRating.placeID].serviceData[restaurantRatings[userRating.placeID].serviceData.length] = parseInt(userRating.service_rating);
-		restaurantRatings[userRating.placeID].foodData[restaurantRatings[userRating.placeID].foodData.length] = parseInt(userRating.food_rating);
-	});
-}
-
-function nothingSelected()
-{
-	hideInfo();
-}
+var consumerRatings = [];
+var ratingLinesRes = [];
+var ratingLinesUser = [];
+var selectedLines = [];
 
 var margin = {top: 45, right: 20, bottom: 30, left: 40},
 	width = 800 - margin.left - margin.right,
@@ -183,7 +53,308 @@ var tip = d3.tip()
 
 svg.call(tip);
   
-function showInfo(restaurant)
+L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+	maxZoom: 18,
+	id: 'examples.map-i875mjb7'
+}).addTo(map);
+
+queue()
+   .defer(d3.csv, "data/geoplaces2.csv")
+   .defer(d3.csv, "data/userprofile.csv")
+   .defer(d3.csv, "data/rating_final.csv")
+   .await(dataLoaded);
+      
+function dataLoaded(error, geoData, users, userRatings)
+{	
+	map.on('click', onMapClick);
+		
+	_.each(geoData, function(value) {
+		value.resLat = value.latitude
+		value.resLon = value.longitude	
+	});
+	_.each(users, function(value) {
+		value.conLat = value.latitude
+		value.conLon = value.longitude	
+	});
+
+	var mergedUserRating = _.map(userRatings, function(item) {
+		return _.extend(item, _.findWhere(users, { userID: item.userID }));
+	});
+	
+	var mergedData = _.map(mergedUserRating, function(item) {
+		return _.extend(item, _.findWhere(geoData, { placeID: item.placeID }));
+	});
+		
+	initRatings(geoData, users);
+	fillRatings(userRatings);
+	createBarChart();
+	nothingSelected();
+	
+	var restaurantSelection = document.getElementById("restaurantSelection");		
+	var bounds = [];
+	var DefaultIcon = L.Icon.Default.extend({
+		options: {
+				iconUrl: 'img/marker-icon-blue.png' 
+		}
+	 });
+	var SelectedIcon = L.Icon.Default.extend({
+		options: {
+				iconUrl: 'img/marker-icon-orange.png' 
+		}
+	 });
+	var UserIcon = L.Icon.Default.extend({
+		options: {
+				iconUrl: 'img/marker-user-green.png' 
+		}
+	 });
+	var UserSelectedIcon = L.Icon.Default.extend({
+		options: {
+				iconUrl: 'img/marker-user-orange.png' 
+		}
+	 });
+	 selectedIcon = new SelectedIcon();
+	 defaultIcon = new DefaultIcon();
+	 userIcon = new UserIcon();
+	 userSelectedIcon = new UserSelectedIcon();
+	 
+	geoData.forEach(function(entry) {
+		var lat = entry.latitude;
+		var lon = entry.longitude;
+		var name = entry.name;
+		var placeID = entry.placeID;
+		var coordinate = L.latLng(lat, lon);
+		
+		var marker = L.marker(coordinate, {
+            data: entry,
+			icon: defaultIcon
+        });
+		marker.addTo(map).on('click', onMapRestaurantClick);
+		bounds[bounds.length] = coordinate;
+		restaurantMarkers[placeID] = marker;
+		ratingLinesRes[placeID] = [];
+		
+		var option = document.createElement("option");
+		option.value = placeID;
+		option.data = entry;
+		option.text = name;
+		restaurantSelection.add(option);
+	});	
+		
+	users.forEach(function(entry) {
+		var lat = entry.latitude;
+		var lon = entry.longitude;
+		var userID = entry.userID;
+		var coordinate = L.latLng(lat, lon);
+		
+		var marker = L.marker(coordinate, {
+            data: entry,
+			icon: userIcon
+        });
+		marker.addTo(map).on('click', onMapUserClick);
+			
+		bounds[bounds.length] = coordinate;
+		userMarkers[userID] = marker;
+		ratingLinesUser[userID] = [];
+	});	
+	
+	userRatings.forEach(function(entry) {
+		var userID = entry.userID;
+		var placeID = entry.placeID;
+		
+		var userMarker = getUserMarker(userID);
+		var restaurantMarker = getRestaurantMarker(placeID);
+	
+		if(userMarker && restaurantMarker)
+		{
+		   var line = [];
+			line.push(userMarker.getLatLng());
+			line.push(restaurantMarker.getLatLng());
+
+			var polyline_options = {
+				color: '#000',
+				weight: 0
+			};
+
+			var polyline = L.polyline(line, polyline_options).addTo(map);
+			
+			ratingLinesRes[placeID][ratingLinesRes[placeID].length] = polyline;
+			ratingLinesUser[userID][ratingLinesUser[userID].length] = polyline;
+		}
+	});	
+	
+	map.fitBounds(bounds);
+}
+
+function onMapClick(e)
+{
+		if(clickedMarker)
+		{
+			clickedMarker.setIcon(defaultIcon);
+		}
+
+		if(clickedUserMarker)
+		{
+			clickedUserMarker.setIcon(userIcon);
+		}
+		
+		nothingSelected();
+}
+
+function onMapRestaurantClick(e)
+{	
+	if(clickedMarker)
+	{
+		clickedMarker.setIcon(defaultIcon);
+	}
+	
+	if(clickedUserMarker)
+	{
+		clickedUserMarker.setIcon(userIcon);
+	}
+	
+	e.target.setIcon(selectedIcon);
+	clickedMarker = e.target;
+	
+	var entry = e.target.options.data;
+	selectedMapMarker(entry);
+	
+	showRestaurantInfo(entry);
+}
+
+function onMapUserClick(e)
+{	
+	if(clickedMarker)
+	{
+		clickedMarker.setIcon(defaultIcon);
+	}
+	if(clickedUserMarker)
+	{
+		clickedUserMarker.setIcon(userIcon);
+	}
+	
+	nothingSelected();
+	
+	
+	e.target.setIcon(userSelectedIcon);
+	clickedUserMarker = e.target;
+	
+	var entry = e.target.options.data;
+	
+	var lines = getUserRatingLines(entry.userID);
+	updateLineColor(lines);
+	
+	showUserInfo(entry);
+}
+
+function selectRestaurant(placeID)
+{
+	if(clickedMarker)
+	{
+		clickedMarker.setIcon(defaultIcon);
+	}
+	
+	var marker = getRestaurantMarker(placeID);
+	if(marker)
+	{
+		marker.setIcon(selectedIcon);
+		map.panTo(marker._latlng);		
+		clickedMarker = marker;
+		showRestaurantInfo(marker.options.data);
+		
+		var lines = getRestaurantRatingLines(placeID);
+		updateLineColor(lines);
+	}
+}
+
+function updateLineColor(newLines)
+{
+	selectedLines.forEach(function(line) {
+		line.setStyle({
+			color: '#000',
+			weight: 0
+			});
+	});
+	
+	newLines.forEach(function(line) {
+		line.setStyle({
+			color: 'red',
+			weight: 2
+			});
+	});
+	
+	selectedLines = newLines;
+}
+
+function getRestaurantMarker(placeID)
+{
+	return restaurantMarkers[placeID];
+}
+
+function getUserMarker(userID)
+{
+	return userMarkers[userID];
+}
+
+function getRestaurantRatingLines(placeID)
+{
+	return ratingLinesRes[placeID];
+}
+
+function getUserRatingLines(userID)
+{
+	return ratingLinesUser[userID];
+}
+
+function selectedMapMarker(restaurant)
+{    
+    var element = document.getElementById('restaurantSelection');
+    element.value = restaurant.placeID;
+
+	showRestaurantInfo(restaurant);
+}
+
+function initRatings(restaurants, users)
+{
+	restaurants.forEach(function(entry) {
+		restaurantRatings[entry.placeID] =  {count:0, overall:0, service:0, food:0, overallData:[], serviceData:[], foodData:[] };
+	});
+
+	users.forEach(function(entry) {
+		var userID = getUserID(entry.userID);
+		consumerRatings[userID] =  {count:0, overall:0, service:0, food:0, overallData:[], serviceData:[], foodData:[] };
+	});
+}
+
+function fillRatings(ratings)
+{
+	ratings.forEach(function(userRating) {
+		restaurantRatings[userRating.placeID].count++;
+		restaurantRatings[userRating.placeID].overall += parseInt(userRating.rating);
+		restaurantRatings[userRating.placeID].service += parseInt(userRating.service_rating);
+		restaurantRatings[userRating.placeID].food += parseInt(userRating.food_rating);
+		
+		restaurantRatings[userRating.placeID].overallData[restaurantRatings[userRating.placeID].overallData.length] = parseInt(userRating.rating);
+		restaurantRatings[userRating.placeID].serviceData[restaurantRatings[userRating.placeID].serviceData.length] = parseInt(userRating.service_rating);
+		restaurantRatings[userRating.placeID].foodData[restaurantRatings[userRating.placeID].foodData.length] = parseInt(userRating.food_rating);
+		
+		var userID = getUserID(userRating.userID);		
+		consumerRatings[userID].count++;
+		consumerRatings[userID].overall += parseInt(userRating.rating);
+		consumerRatings[userID].service += parseInt(userRating.service_rating);
+		consumerRatings[userID].food += parseInt(userRating.food_rating);
+		
+		consumerRatings[userID].overallData[consumerRatings[userID].overallData.length] = parseInt(userRating.rating);
+		consumerRatings[userID].serviceData[consumerRatings[userID].serviceData.length] = parseInt(userRating.service_rating);
+		consumerRatings[userID].foodData[consumerRatings[userID].foodData.length] = parseInt(userRating.food_rating);
+	});
+}
+
+function nothingSelected()
+{
+	hideInfo();
+}
+
+function showRestaurantInfo(restaurant)
 {	
 	updateElementText("restaurantHeader", restaurant.name);
 	updateElementText("address", restaurant.address);
@@ -198,33 +369,60 @@ function showInfo(restaurant)
 	updateElementText("price", restaurant.price);
 	updateElementText("url", restaurant.url != "?" ? "<a href='http://" + restaurant.url  +"' target='_blank' >" + restaurant.url + "</a>" : "?");
 	
-	showRating(restaurant.placeID);
+	document.getElementById("restaurant-information").style.visibility = "visible";
+	document.getElementById("user-information").style.visibility = "hidden";
+	
+	var rating = restaurantRatings[restaurant.placeID];
+	showRating(rating);
+	
+	var lines = getRestaurantRatingLines(restaurant.placeID);
+	updateLineColor(lines);
 }
 
-function hideInfo(){
-	updateElementText("restaurantHeader", "-");
-	updateElementText("address", "-");
-	updateElementText("city", "-");
-	updateElementText("state", "-");
-	updateElementText("country", "-");
-	updateElementText("zip", "-");
-	updateElementText("alcohol", "-");
-	updateElementText("smoking_area", "-");
-	updateElementText("dress_code", "-");
-	updateElementText("accessibility", "-");
-	updateElementText("price", "-");
-	updateElementText("url", "-");
+function showUserInfo(user)
+{
+	updateElementText("smoker", user.smoker);
+	updateElementText("drink_level", user.drink_level);
+	updateElementText("dress_preference", user.dress_preference);
+	updateElementText("ambience", user.ambience);
+	updateElementText("transport", user.transport);
+	updateElementText("marital_status", user.marital_status);
+	updateElementText("birth_year", user.birth_year);
+	updateElementText("interest", user.interest);
+	updateElementText("personality", user.personality);
+	updateElementText("religion", user.religion);
+	updateElementText("activity", user.activity);
+	updateElementText("color", user.color);
+	updateElementText("weight", user.weight);
+	updateElementText("budget", user.budget);
+	updateElementText("height", user.height);
+	
+	
+	document.getElementById("user-information").style.visibility = "visible";
+	document.getElementById("restaurant-information").style.visibility = "hidden";
+	
+	var userID = getUserID(user.userID);
+	var rating = consumerRatings[userID];
+	showRating(rating);	
+}
+
+function hideInfo()
+{	
+	document.getElementById("user-information").style.visibility = "hidden";
+	document.getElementById("restaurant-information").style.visibility = "hidden";
 	
 	hideRating();
+	
+	updateLineColor([]);
 }
 
-function updateElementText(elementName, value){
+function updateElementText(elementName, value)
+{
 	document.getElementById(elementName).innerHTML = (value != "?" ? value : "-");
 }
 
-function showRating(placeID)
+function showRating(rating)
 {
-	var rating = restaurantRatings[placeID];
 	var barArray = [];
 	barArray[0] = ["Overall", rating.overall / rating.count, standardDeviation(rating.overallData)];
 	barArray[1] = ["Food", rating.food / rating.count, standardDeviation(rating.foodData)];
@@ -241,6 +439,15 @@ function hideRating()
 	barArray[2] = ["Service", 0, 0, 0];
 	barArray[3] = ["Count", 0];
 	updateBarChart(barArray);
+}
+
+function getUserID(userID)
+{
+	if(userID)
+	{
+		userID = userID.substr(1);
+		return parseInt(userID);
+	}
 }
 
 function createBarChart()
@@ -318,7 +525,8 @@ function updateBarChart(ratingArray)
       .text("Ratings: " + ratingArray[3][1]);
 }
 
-function standardDeviation(values){
+function standardDeviation(values)
+{
   var avg = average(values);
   
   var squareDiffs = values.map(function(value){
@@ -333,7 +541,8 @@ function standardDeviation(values){
   return stdDev;
 }
  
-function average(data){
+function average(data)
+{
   var sum = data.reduce(function(sum, value){
     return sum + value;
   }, 0);
